0ee981cdc09cc17e4f1fa31dd4a9444d9b9ca041
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2023-05-02 13:08:04 +0530

    added logout functionality

--- frontendtask/src/components/Navbar.jsx
+++ frontendtask/src/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
 import React from 'react'
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+    const navigate = useNavigate();
     const links = [
         {
             to: "/",
@@ -29,6 +30,11 @@ const Navbar = () => {
         textDecoration: "none",
         color: "black",
     };
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('email');
+        navigate('/login');
+    };
     return (
         <Box mb={["10%", "10%", "4%"]}>
             <Box display={{ base: "flex", sm: 'flex', lg: "flex" }} boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px;" position={"fixed"} top="0.1px" zIndex={"100"} bg="white" justifyContent="space-around" w="100%" padding={"10px"}>
@@ -43,6 +49,7 @@ const Navbar = () => {
                         {ele.title}
                     </NavLink>
                 )}
+                <Button variant={'outline'} color={'white'} bg='black' onClick={handleLogout}>Logout</Button>
             </Box>
         </Box>
     );
